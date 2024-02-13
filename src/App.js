87376c7258a4dd04d6d17dc2ab7d1df211cd9dd7
atulyaraaja <atulraj1010@tutanota.com>
2024-02-13 09:56:42 +0530

    Home view

--- src/App.js
+++ src/App.js
@@ -1,21 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+
+import Home from './views/Home';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
-
-
+      <Home/>
     </div>
-
-
-
-
-
-
   );
 }
 
